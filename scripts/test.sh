03b53d6bd7866f39e07d9f0a3226ab505f30da25
Nathan <nathanwallh@gmail.com>
2017-05-16 19:11:27 +0300

    fixed

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,19 +3,19 @@
 
 ## Domains and problems
 
-PUZZLE_DOM=/home/nathan/planner/tile/puzzle.pddl
-PUZZLE_PROB1=/home/nathan/planner/tile/puzzle01.pddl
-PUZZLE_PROB2=/home/nathan/planner/tile/puzzle02.pddl
-PUZZLE_RLX=/home/nathan/planner/tile/puzzle_rlx.pddl
+PUZZLE_DOM=../problems/tile/domain.pddl
+PUZZLE_PROB1=../problems/tile/puzzle01.pddl
+PUZZLE_PROB2=../problems/tile/puzzle02.pddl
+PUZZLE_RLX=../problems/tile/puzzle_rlx.pddl
 
-KNIGHTS_DOM=/home/nathan/planner/knights_tour/knights_tour.pddl
-KNIGHTS_PROB1=/home/nathan/planner/knights_tour/knights_tour5.pddl
-KNIGHTS_PROB2=/home/nathan/planner/knights_tour/knights_tour8.pddl
+KNIGHTS_DOM=../problems/knights_tour/knights_tour.pddl
+KNIGHTS_PROB1=../problems/knights_tour/knights_tour5.pddl
+KNIGHTS_PROB2=../problems/knights_tour/knights_tour8.pddl
 
-HIKING_DOM=/home/nathan/planner/TESTING/seq-sat/Hiking/domain.pddl
-HIKING_PROB1=/home/nathan/planner/TESTING/seq-sat/Hiking/ptesting-3-4-6.pddl
-HIKING_PROB2=/home/nathan/planner/TESTING/seq-sat/Hiking/ptesting-3-4-7.pddl
-HIKING_PROB3=/home/nathan/planner/TESTING/seq-sat/Hiking/ptesting-3-4-8.pddl
+HIKING_DOM=../problems/TESTING/Hiking/domain.pddl
+HIKING_PROB1=../problems/TESTING/Hiking/ptesting-3-4-6.pddl
+HIKING_PROB2=../problems/TESTING/Hiking/ptesting-3-4-7.pddl
+HIKING_PROB3=../problems/TESTING/Hiking/ptesting-3-4-8.pddl
 
 ## Heuristics
 
@@ -31,9 +31,9 @@ LMCUT_H="lmcut()"
 PDB_H="pdb(patterns=[[1,2],[3]])"
 CPDB_H="cpdbs(genetic(pdb_max_size=50000, num_collections=5, num_episodes=10, mutation_probability=0.01, disjoint=false, random_seed=-1))"
 #systematic(pattern_max_size=3, only_interesting_patterns=true)
-/home/nathan/planner/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB2 --search "lazy(tiebreaking([$MY_H]),max_time=10)"
+../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB2 --search "lazy(tiebreaking([$MY_H]),max_time=10)"
 
-#/home/nathan/planner/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy_wastar([$IPDB_H,$FF_H,$GC_H],w=2,max_time=10)"
-#/home/nathan/planner/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB2 --search "astar($CPDB_H,max_time=60)"
-#/home/nathan/planner/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy_greedy($ADD_H,max_time=5)"
-#/home/nathan/planner/fast-downward.py $KNIGHTS_DOM $KNIGHTS_PROB2 --search "lazy(tiebreaking([$FF_H,$GC_H,$PDB_H]),max_time=5)"
+#../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy_wastar([$IPDB_H,$FF_H,$GC_H],w=2,max_time=10)"
+#../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB2 --search "astar($CPDB_H,max_time=60)"
+#../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy_greedy($ADD_H,max_time=5)"
+#../fast-downward/fast-downward.py $KNIGHTS_DOM $KNIGHTS_PROB2 --search "lazy(tiebreaking([$FF_H,$GC_H,$PDB_H]),max_time=5)"
