ee7df78adf56862380fae8a821be0aed0f4d8a97
louiscyphre <dev.cccp@gmail.com>
2017-05-22 23:47:29 +0300

    Preparing to update from average hdb branch

--- scripts/test.sh
+++ scripts/test.sh
@@ -19,20 +19,34 @@ HIKING_PROB3=../problems/Hiking/ptesting-3-4-8.pddl
 
 ## Heuristics
 
-MAX_H="hmax()"
+MY_H="my_heuristic(tz_first = 203304688, tz_second = 320934904 )"
+#admissible
+BLIND_H="blind()"
+LMCOUNT_A_H="lmcount(lm_hm(m=2),admissible=true)"
+MERGE_H="merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false),merge_strategy=merge_stateless(merge_selector=score_based_filtering(scoring_functions=[goal_relevance,dfp,total_order])),label_reduction=exact(before_shrinking=true,before_merging=false),max_states=50000,threshold_before_merge=1)"
+CPDB_H="cpdbs(genetic(pdb_max_size=50000, num_collections=5, num_episodes=10, mutation_probability=0.01, disjoint=false, random_seed=-1))"
+#not admissible
 FF_H="ff()"
 GC_H="goalcount()"
 IPDB_H="ipdb()"
 ADD_H="add()"
-MY_H="my_heuristic(tz_first = 203304688, tz_second = 320934904 )"
-BLIND_H="blind()"
 HM_H="hm()"
 LMCUT_H="lmcut()"
-PDB_H="pdb(patterns=[[1,2],[3]])"
 LMCOUNT_H="lmcount(lm_hm(m=1))"
-CPDB_H="cpdbs(genetic(pdb_max_size=50000, num_collections=5, num_episodes=10, mutation_probability=0.01, disjoint=false, random_seed=-1))"
+PDB_H="pdb(patterns=[[1,2],[3]])"
+LMCOUNT_H="lmcount(lm_hm(m=2))"
+MAX_H="hmax()"
 #systematic(pattern_max_size=3, only_interesting_patterns=true)
-../fast-downward/fast-downward.py $HIKING_DOM $PUZZLE_PROB1 --search "lazy(tiebreaking([$LMCOUNT_H,$GC_H,$FF_H]),reopen_closed=true,max_time=120)"
+../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy(tiebreaking([$LMCOUNT_A_H, $BLIND_H]),reopen_closed=true,max_time=10)" >out.out 2>&1
+./cat_db.sh
+../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy(tiebreaking([$MERGE_H, $IPDB_H]),reopen_closed=true,max_time=10)"  >>out.out 2>&1
+./cat_db.sh
+../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy(tiebreaking([$CPDB_H, $LMCUT_H]),reopen_closed=true,max_time=10)"  >>out.out 2>&1
+./cat_db.sh
+#../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy(tiebreaking([$FF_H]),reopen_closed=true,max_time=20)"
+../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy(alt([single($MY_H),single($FF_H)]),reopen_closed=true,max_time=20)"
+#../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy_wastar([$MY_H, $FF_H],w=2,max_time=20)"
+
 #../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy_wastar($MY_H,w=2,max_time=120)"
 #../fast-downward/fast-downward.py $HIKING_DOM $HIKING_PROB2 --search "lazy(tiebreaking([$LMCOUNT_H,$FF_H]),max_time=20)"
 #../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy(alt([single($ADD_H),single($FF_H)]),reopen_closed=true,max_time=20)"
