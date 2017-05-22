84594ae19a9d7cdfcec7d27727ab54ea8914256e
Nathan <nathanwallh@gmail.com>
2017-05-22 15:06:36 +0300

    deleted old print from trace_path

--- fast-downward/src/search/search_space.cc
+++ fast-downward/src/search/search_space.cc
@@ -131,9 +131,6 @@ void SearchSpace::trace_path(const GlobalState &goal_state,
     assert(path.empty());
     for (;;) {
         const SearchNodeInfo &info = search_node_infos[current_state];
-        ///////////////////////////
-        std::cout << to_string( current_state.get_hash() ) + " " + to_string( info.g ) <<endl;
-        //////////////////////////
         if (info.creating_operator == -1) {
             assert(info.parent_state_id == StateID::no_state);
             break;
