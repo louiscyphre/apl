6f16626642fce5a155d3e48e3d602406431fc4cb
Nathan <nathanwallh@gmail.com>
2017-05-26 17:18:01 +0300

    added test.sh to gitignore

--- scripts/test.sh
+++ scripts/test.sh
@@ -46,17 +46,7 @@ BLIND_H="blind()"
 HM_H="hm()"
 LMCUT_H="lmcut()"
 PDB_H="pdb(patterns=[[1,2],[3]])"
-<<<<<<< HEAD
-CPDB_H="cpdbs(genetic(pdb_max_size=50000, num_collections=5, num_episodes=10, mutation_probability=0.01, disjoint=false, random_seed=-1))"
-#systematic(pattern_max_size=3, only_interesting_patterns=true)
-../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy(sginel(max([$MAX_H,$GC_H,$IPDB_H])),reopen_closed=true,max_time=20)"
-#../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy_wastar([$MY_H],w=2,max_time=20)"
 
-#../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy_wastar([$IPDB_H,$FF_H,$GC_H],w=2,max_time=10)"
-#../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB2 --search "astar($CPDB_H,max_time=60)"
-#../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy_greedy($ADD_H,max_time=5)"
-#../fast-downward/fast-downward.py $KNIGHTS_DOM $KNIGHTS_PROB2 --search "lazy(tiebreaking([$FF_H,$GC_H,$PDB_H]),max_time=5)"
-=======
 LMCOUNT_H="lmcount(lm_rhw(),pref=true)"
 LMCOUNTAD_H="lmcount(lm_rhw(),pref=true,admissible=true)"
 CPDB_H="cpdbs(systematic())"
@@ -78,4 +68,3 @@ CPDB_H="cpdbs(systematic())"
 #../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "lazy(alt([tiebreaking([$LMCOUNT_H,$FF_H]),tiebreaking([$FF_H,sum($GC_H,g())])]),max_time=120)"
 #../fast-downward/fast-downward.py $BARMAN_DOM $BARMAN_PROB2 --search "lazy(alt([tiebreaking([$LMCOUNT_H,$FF_H]),tiebreaking([$CPDB_H,sum($GC_H,g())])]),max_time=120)"
 #../fast-downward/fast-downward.py $PUZZLE_DOM $PUZZLE_PROB1 --search "iterated([lazy(tiebreaking([$LMCOUNT_H,$ADD_H,$GC_H])),lazy_wastar($FF_H,w=2)],max_time=120)"
->>>>>>> stateDB2
